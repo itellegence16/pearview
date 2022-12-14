30d4cd68852c3317376654f8475cb770c99a97db
sana.mahajan <sana.mahajan@clairvoyantsoft.com>
2022-12-14 15:45:53 +0530

    Committing all the files

--- config/mapping_data.py
+++ config/mapping_data.py
@@ -5,8 +5,8 @@ from pyspark.sql.functions import when,split
 import os,json
 #table_details=str(input("Enter the table name: e.g. student, course, fee: ")).lower()
 table_details="student"
-source_dir="C:\My things\EXL\CLA-DataLake\\"+table_details+"\\"
-config_dir="C:\My things\EXL\CLA-DataLake\config\\"
+source_dir=r"C:\Users\Sana Mahajan\Documents\git_practice\pearview\\"+table_details+"\\"
+config_dir=r"C:\Users\Sana Mahajan\Documents\git_practice\pearview\config\\"
 source_dir_length=len(os.listdir(   source_dir))
 target_file=config_dir+"target_mapped_data.csv"
 col_csv=config_dir+"col.csv"
@@ -21,10 +21,13 @@ def read_source_data():
     csv_col=df_col.to_csv(col_csv,index=False)
     df_target = spark.read.csv(col_csv,header=True)
     for file in os.listdir(source_dir):
-        print(file)
         df_source = spark.read.csv(source_dir+file,header=True)
         df_target=transform_source_data(df_source,df_target)
+    ### This is to store output data in csv for future use##
     df_target.toPandas().to_csv(target_file,index=False)
+    print(df_target.columns)
+    print(df_target.show())
+
 
 def transform_source_data(df_source,df_target):
     source_column_names = list(df_source.columns)
@@ -41,8 +44,9 @@ def transform_source_data(df_source,df_target):
             df_source=df_source.withColumnRenamed(common_column,key)
             #print(df_source.columns)
             if(key=="gender"):
-                df_source=df_source.withColumn("gender", when(df_source.gender == "M", "Male").when(df_source.gender == "F", "Female")\
-                            .otherwise(df_source.gender))
+                df_source=df_source.withColumn("gender", when((df_source.gender == "M") | (df_source.gender == "Male") | (df_source.gender == "Men"), "Male")\
+                                               .when((df_source.gender == "F") | (df_source.gender == "Female") | (df_source.gender == "Women"), "Female")\
+                        .otherwise(df_source.gender))
     #### To input source column data into target columns####
     result=df_source.unionByName(df_target,allowMissingColumns=True)
     ##
